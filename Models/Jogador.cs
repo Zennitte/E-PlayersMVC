f00b2692e34d97c28d0b7bb9e27a012a696a0e2a
Kauê Camargo <camargokaue92@gmail.com>
2021-06-15 18:27:54 -0300

    Model Equipe Feita

---
+++ Models/Jogador.cs
@@ -0,0 +1,10 @@
+namespace E_PlayersMVC.Models
+{
+    public class Jogador
+    {
+        public int IdJogador { get; set; }
+        public string Nome { get; set; }
+        public int IdEquipe { get; set; }
+        
+    }
+}
---
+++ Models/Noticias.cs
@@ -0,0 +1,10 @@
+namespace E_PlayersMVC.Models
+{
+    public class Noticias
+    {
+        public int IdNoticia { get; set; }
+        public string Titulo { get; set; }
+        public string Texto { get; set; }
+        public string Imagem { get; set; }
+    }
+}
